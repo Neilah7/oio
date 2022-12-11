e48e07fa92ba6e8200a57878e5a8b2296635da89
Maryam <107400570+Neilah7@users.noreply.github.com>
2022-12-11 16:55:16 +0300

    Initial Commit

---
+++ oio/oioApp.swift
@@ -0,0 +1,17 @@
+//
+//  oioApp.swift
+//  oio
+//
+//  Created by maryam almijlad on 11/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct oioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
